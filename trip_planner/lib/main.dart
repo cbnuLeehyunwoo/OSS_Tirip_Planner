953d4cf9950df47abf374c07dbbeb3cac80c0616
skinheadman_sec <zzzoad929@gmail.com>
2025-08-19 18:38:50 +0900

    Feat:Chat screen feature with gemma3

--- trip_planner/lib/main.dart
+++ trip_planner/lib/main.dart
@@ -3,9 +3,9 @@ import 'TextEdit/travel_home_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  
+
   runApp(const TravelApp());
 }
 
