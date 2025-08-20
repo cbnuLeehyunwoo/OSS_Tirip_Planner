aa1e7ffb4cc79c3dc38b1332493fd5b4e95a7f14
taehyun <stest12345s@gmail.com>
2025-08-21 01:01:44 +0900

    add DetailData Function

--- lib/tour_api.dart
+++ lib/tour_api.dart
@@ -10,11 +10,94 @@ Future<List<dynamic>> getTourData(String area, String sigungu) async {
     'apis.data.go.kr', // 도메인
     '/B551011/KorService2/areaBasedList2', // 경로
     {
-      'numOfRows': '10',
+      'numOfRows': '20',
       'pageNo': '1',
       'MobileOS': 'AND',
       'MobileApp': appName,
       '_type': 'json',
+      'contentTypeId': '12', // 관광지 타입 (12:관광지,14:문화시설,15:축제공연행사,25:여행코스,28:레포츠,32:숙박,38:쇼핑,39:음식점)
+      'areaCode': area,
+      'sigunguCode': sigungu,
+      'serviceKey': apiKey, 
+    },
+  );
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    // 성공적으로 데이터를 받아왔을 때
+    final data = json.decode(utf8.decode(response.bodyBytes));
+    final items = data['response']['body']['items']['item'];
+    if (items == null){
+      return []; //검색 결과가 없을 때 빈 리스트 반환
+    }
+    else if(items is List){
+      return items;// 결과가 여러 개일 경우 그대로 반환
+    }
+    else{
+      return [items]; // 결과가 1개여서 Map인 경우, 리스트에 담아서 반환
+    }
+    
+  } else {
+    // 오류 발생 시
+    throw Exception('Failed to load tour data: ${response.statusCode}');
+  }
+}
+
+Future<List<dynamic>> getRestaurantData(String area, String sigungu) async {
+
+  final apiKey = dotenv.env['TOUR_API_KEY']; // 발급받은 API 키
+  final appName = 'tourPlaner';
+  final url = Uri.https(
+    'apis.data.go.kr', // 도메인
+    '/B551011/KorService2/areaBasedList2', // 경로
+    {
+      'numOfRows': '20',
+      'pageNo': '1',
+      'MobileOS': 'AND',
+      'MobileApp': appName,
+      '_type': 'json',
+      'contentTypeId': '39', // 관광지 타입 (12:관광지,14:문화시설,15:축제공연행사,25:여행코스,28:레포츠,32:숙박,38:쇼핑,39:음식점)
+      'areaCode': area,
+      'sigunguCode': sigungu,
+      'serviceKey': apiKey, 
+    },
+  );
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    // 성공적으로 데이터를 받아왔을 때
+    final data = json.decode(utf8.decode(response.bodyBytes));
+    final items = data['response']['body']['items']['item'];
+    if (items == null){
+      return []; //검색 결과가 없을 때 빈 리스트 반환
+    }
+    else if(items is List){
+      return items;// 결과가 여러 개일 경우 그대로 반환
+    }
+    else{
+      return [items]; // 결과가 1개여서 Map인 경우, 리스트에 담아서 반환
+    }
+    
+  } else {
+    // 오류 발생 시
+    throw Exception('Failed to load tour data: ${response.statusCode}');
+  }
+}
+
+Future<List<dynamic>> getAccommodationData(String area, String sigungu) async {
+
+  final apiKey = dotenv.env['TOUR_API_KEY']; // 발급받은 API 키
+  final appName = 'tourPlaner';
+  final url = Uri.https(
+    'apis.data.go.kr', // 도메인
+    '/B551011/KorService2/areaBasedList2', // 경로
+    {
+      'numOfRows': '20',
+      'pageNo': '1',
+      'MobileOS': 'AND',
+      'MobileApp': appName,
+      '_type': 'json',
+      'contentTypeId': '32', // 관광지 타입 (12:관광지,14:문화시설,15:축제공연행사,25:여행코스,28:레포츠,32:숙박,38:쇼핑,39:음식점)
       'areaCode': area,
       'sigunguCode': sigungu,
       'serviceKey': apiKey, 
@@ -77,4 +160,45 @@ Future<List<dynamic>> getAreaCodes({String? areaCode}) async {
   }
   // 실패하거나 데이터가 없으면 빈 리스트 반환
   return [];
+}
+
+
+Future<dynamic> getDetailData(String contentID,String contentTypeId) async {
+
+  final apiKey = dotenv.env['TOUR_API_KEY']; // 발급받은 API 키
+  final appName = 'tourPlaner';
+  final url = Uri.https(
+    'apis.data.go.kr', // 도메인
+    '/B551011/KorService2/detailIntro2', // 경로
+    {
+      'MobileOS': 'AND',
+      'MobileApp': appName,
+      '_type': 'json',
+      'contentId': contentID,
+      'contentTypeId': contentTypeId, // 관광지 타입 (12:관광지,14:문화시설,15:축제공연행사,25:여행코스,28:레포츠,32:숙박,38:쇼핑,39:음식점)
+      'numOfRows': '20',
+      'pageNo': '1',
+      'serviceKey': apiKey, 
+    },
+  );
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    // 성공적으로 데이터를 받아왔을 때
+    print('API 호출 성공: ${response.statusCode}');
+    final data = json.decode(utf8.decode(response.bodyBytes));
+    final items = data['response']['body']['items']['item'];
+    if (items == null){
+      return []; //검색 결과가 없을 때 빈 리스트 반환
+    }
+    else if(items is List){
+      return items;// 결과가 여러 개일 경우 그대로 반환
+    }
+    else{
+      return [items]; // 결과가 1개여서 Map인 경우, 리스트에 담아서 반환
+    }
+  } else {
+    // 오류 발생 시
+    throw Exception('Failed to load tour data: ${response.statusCode}');
+  }
 }
